404a5f64aff7cd18e87f3a5a1f7cba7ff2d5c9a6
Johan <johandhaneja2568@gmail.com>
2026-08-15 11:24:10 +0530

    feat: expose AI intent interpreter

--- services/intent/src/pyrintu_intent/__init__.py
+++ services/intent/src/pyrintu_intent/__init__.py
@@ -1,7 +1,10 @@
+from .ai_interpreter import AIIntentInterpreter, AITextGenerator
 from .models import Intent, IntentState, ParsedIntent
 from .service import IntentService, IntentValidationError, InMemoryIntentRepository
 
 __all__ = [
+    "AIIntentInterpreter",
+    "AITextGenerator",
     "Intent",
     "IntentState",
     "ParsedIntent",
